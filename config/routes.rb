6618b4a6d763d747e54e0bfdcdd052935b4ccbc7
sohansalve <sohan@webkorps.com>
2022-06-01 18:12:26 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/articles/:id", to: "articles#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
